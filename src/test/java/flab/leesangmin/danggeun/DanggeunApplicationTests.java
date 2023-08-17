10e791ad3116407a6d6fad17d24b5ee9852e5594
IMWoo94 <leemeawoo94@gmail.com>
2023-08-17 23:24:21 +0900

    feat #2 danggeun-2 [프로젝트 세팅] 이상민 프로젝트 세팅 springboot 2.7.14[Gradle - Groovy][Java11] retry
    closes #2
    fixed #2
    fixes #2

---
+++ src/test/java/flab/leesangmin/danggeun/DanggeunApplicationTests.java
@@ -0,0 +1,13 @@
+package flab.leesangmin.danggeun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanggeunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
