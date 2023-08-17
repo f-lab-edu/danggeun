c73302e982db0672ea1f03bf624bc39ee880d8e6
IMWoo94 <leemeawoo94@gmail.com>
2023-08-17 14:05:47 +0900

    feat#2 이상민 프로젝트 세팅 danggeun-leesangmin [프로젝트 세팅] 이상민 프로젝트 세팅 springboot 2.7.14[Gradle - Groovy][Java11]

---
+++ src/main/java/flab/leesangmin/danggeun/DanggeunApplication.java
@@ -0,0 +1,13 @@
+package flab.leesangmin.danggeun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanggeunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanggeunApplication.class, args);
+	}
+
+}
