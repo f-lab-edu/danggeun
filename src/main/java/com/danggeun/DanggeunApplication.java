bca561e78744f6168f83bb05a3388aa3d9b98bf5
sunbeom <tnsqja012@naver.com>
2023-08-29 18:21:19 +0900

    danggeun-6 feat #5 danggeun-3 [프로젝트 세팅] 권순범 프로젝트 세팅 java 파일 추가, 삭제

--- src/main/java/com/danggeun/DanggeunApplication.java
+++ src/main/java/com/danggeun/DanggeunApplication.java
@@ -1,13 +1,14 @@
-package com.example.demo;
+package com.danggeun;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DanggeunApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(DanggeunApplication.class, args);
 	}
 
 }
