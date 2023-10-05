6be932043a4256b75763e7161e9a895ab00fd3aa
IMWoo94 <leemeawoo94@gmail.com>
2023-10-05 17:21:10 +0900

    feature/#13-article-write 이슈 : #13 게시글 기능
    1. test 시 앞으로 중복 사용될 듯한 어노테이션 하나의 어노테이션으로 생성하여 사용 처리.

---
+++ src/test/java/com/danggeun/annotation/SpringIntegrationTest.java
@@ -0,0 +1,20 @@
+package com.danggeun.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@ActiveProfiles("local")
+@SpringBootTest
+@Transactional
+public @interface SpringIntegrationTest {
+}
