983170cf305d91a44a69acc39209bd62035f471e
sunbeom <tnsqja012@naver.com>
2023-11-07 17:01:46 +0900

    danggeun-9 회원가입 기능 #9 회원가입 기능

--- src/main/java/com/danggeun/region/repository/JdbcTemplateRegionRepository.java
+++ src/main/java/com/danggeun/region/repository/JdbcTemplateRegionRepository.java
@@ -1,31 +1,46 @@
 package com.danggeun.region.repository;
 
-import org.springframework.jdbc.core.JdbcTemplate;
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
-import com.danggeun.user.dto.UserDTO;
+import com.danggeun.region.dto.RegionDTO;
 
-import lombok.RequiredArgsConstructor;
-
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 @Repository
 public class JdbcTemplateRegionRepository implements RegionRepository {
-	private final JdbcTemplate jdbcTemplate;
+	private final SimpleJdbcInsert simpleJdbcInsert;
+
+	public JdbcTemplateRegionRepository(DataSource dataSource) {
+		this.simpleJdbcInsert = new SimpleJdbcInsert(dataSource)
+			.withTableName("user_region")
+			.usingGeneratedKeyColumns("region_id")
+			.usingColumns("user_id", "represent_region_status", "region_range_status", "longitude", "latitude",
+				"active", "created_id", "modified_id");
+	}
 
 	/**
 	 * 사용자 지역 정보 생성
-	 * @param userDTO
+	 * @param regionDTO
+	 * @return regionDTO
 	 */
 	@Override
-	public void save(UserDTO userDTO) {
-		StringBuilder query = new StringBuilder();
-		query.append("insert into ")
-			.append("user_region(user_id, represent_region_status, region_range_status, ")
-			.append("longitude, latitude, active, created_date, ")
-			.append("created_id, modified_date, modified_id) ")
-			.append("values (?, ?, ?, ?, ?, ?, now(), ?, now(), ?)");
-		jdbcTemplate.update(query.toString(), userDTO.getUserId(), userDTO.getRepresentRegionStatus(),
-			userDTO.getRegionRangeStatus(), userDTO.getLongitude(), userDTO.getLatitude(), userDTO.isUserActive(),
-			userDTO.getCreateId(), userDTO.getModifiedId());
+	public RegionDTO save(RegionDTO regionDTO) {
+		SqlParameterSource param = new MapSqlParameterSource()
+			.addValue("user_id", regionDTO.getUserId())
+			.addValue("represent_region_status", regionDTO.getRepresentRegionStatus())
+			.addValue("region_range_status", regionDTO.getRegionRangeStatus())
+			.addValue("longitude", regionDTO.getLongitude())
+			.addValue("latitude", regionDTO.getLatitude())
+			.addValue("active", regionDTO.isUserActive())
+			.addValue("created_id", regionDTO.getCreatedId())
+			.addValue("modified_id", regionDTO.getModifiedId());
+		Number number = simpleJdbcInsert.executeAndReturnKey(param);
+
+		regionDTO.setRegionId((Long)number);
+		return regionDTO;
 	}
 }
