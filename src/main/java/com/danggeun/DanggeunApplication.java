9986266975880ae48314804aea6c35a132738ee3
IMWoo94 <leemeawoo94@gmail.com>
2023-11-08 21:57:53 +0900

    feature/#50-JPA-init
    이슈 : #50 Spring Data JPA 및 Auditing 적용
    
    1. auditing 기능 활성화

--- src/main/java/com/danggeun/DanggeunApplication.java
+++ src/main/java/com/danggeun/DanggeunApplication.java
@@ -2,9 +2,10 @@ package com.danggeun;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class DanggeunApplication {
 
 	public static void main(String[] args) {
