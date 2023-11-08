73c24c3101b1c5f3b1d2ecabdaf04e143c6af497
IMWoo94 <leemeawoo94@gmail.com>
2023-11-08 21:57:24 +0900

    feature/#50-JPA-init
    이슈 : #50 Spring Data JPA 및 Auditing 적용
    
    1. 등록자, 수정자의 경우 auditing 적용 및 audting config 설정

---
+++ src/main/java/com/danggeun/commons/config/AuditingConfig.java
@@ -0,0 +1,21 @@
+package com.danggeun.commons.config;
+
+import java.util.Optional;
+import java.util.Random;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuditingConfig {
+
+	@Bean
+	public AuditorAware<Long> auditorProvider() {
+		// 추후 세션 사용자 정보의 값으로 변경 할 수 있도록 제공
+		// 현재는 Seed 값을 동일하게 주어 동일한 난수 패턴이 발생 하도록 하여 테스트
+		Random random = new Random(5);
+		return () -> Optional.of(random.nextLong());
+	}
+
+}
--- src/main/java/com/danggeun/commons/entity/BaseEntity.java
+++ src/main/java/com/danggeun/commons/entity/BaseEntity.java
@@ -1,16 +1,24 @@
 package com.danggeun.commons.entity;
 
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 
+@EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
 @Getter
 public class BaseEntity extends BaseTimeEntity {
 
+	@CreatedBy
 	@Column(updatable = false)
-	private String registeredId;
+	private Long registeredId;
 
-	private String modifiedId;
+	@LastModifiedBy
+	private Long modifiedId;
 
 }
