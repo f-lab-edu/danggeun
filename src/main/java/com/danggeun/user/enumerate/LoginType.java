7846c5e20f60989f4e6832a63aa148844b66d659
sunbeom <tnsqja012@naver.com>
2023-11-15 16:14:34 +0900

    feature/#49-user-social-login #49 소셜 로그인 기능 1. 소셜 로그인 인증 후 사용자 정보 처리 추가 2. oauth2EmailInvalidException 추가 3. LoginType enum class 수정 4. SessionConst class 추가

---
+++ src/main/java/com/danggeun/user/enumerate/LoginType.java
@@ -0,0 +1,14 @@
+package com.danggeun.user.enumerate;
+
+public enum LoginType {
+	/**
+	 *	DEFALUT : 기본 로그인
+	 *	NAVER : 네이버 로그인
+	 *  KAKAO : 카카오 로그인
+	 *  GOOGLE : 구글 로그인
+	 */
+	DEFAULT,
+	NAVER,
+	KAKAO,
+	GOOGLE
+}
