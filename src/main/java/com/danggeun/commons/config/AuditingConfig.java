940bae1e5f15b591cdacafc016ece0d0d676dee5
IMWoo94 <leemeawoo94@gmail.com>
2023-11-26 14:46:26 +0900

    feature#50-JPA-init refactor : #50 Spring Data JPA 및 Auditing 적용
    AuditingConfig 로 @EnableJpaAuditing 옮김

--- src/main/java/com/danggeun/commons/config/AuditingConfig.java
+++ src/main/java/com/danggeun/commons/config/AuditingConfig.java
@@ -5,9 +5,11 @@ import java.util.Random;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.stereotype.Component;
 
 @Component
+@EnableJpaAuditing
 public class AuditingConfig {
 
 	@Bean
